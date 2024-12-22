ea35e5672822252c24abb728f5efaea8acb69dda
jedsantillan <jedsantillan811@gmail.com>
2024-12-22 20:40:45 +0800

    implemented todo-list component

---
+++ src/app/presentation/todo-list/todo-list/todo-list.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from "@angular/common";
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from "@angular/material/icon";
+import { MatButton } from "@angular/material/button";
+import { MatListItem } from '@angular/material/list';
+import { MatFormField } from '@angular/material/form-field';
+import { MatCheckbox } from '@angular/material/checkbox';
+import { Todo } from '../../../models/todo.model';
+
+@Component({
+  selector: 'app-todo-list',
+  templateUrl: './todo-list.component.html',
+  styleUrls: ['./todo-list.component.scss'],
+  standalone: true,
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatIconModule,
+    MatButton,
+    MatListItem,
+    MatFormField,
+    MatCheckbox,
+]
+})
+export class TodoListComponent implements OnInit {
+  todos: Todo[] = [];
+
+  constructor() { }
+
+  ngOnInit() {
+    this.loadTodo();
+  }
+
+  loadTodo(){
+    this.todos.push({id: 1, name: 'initial todo item', isDone: false })
+  }
+
+  addTodo(name: string){
+
+  }
+
+  deleteTodo(id: number) {
+
+  }
+
+}
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,14 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { TodoListComponent } from './app/presentation/todo-list/todo-list/todo-list.component';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
-bootstrapApplication(AppComponent, appConfig)
+// bootstrapApplication(AppComponent, appConfig)
+//   .catch((err) => console.error(err));
+
+  bootstrapApplication(TodoListComponent, {
+    providers: [provideHttpClient(withFetch()), provideAnimationsAsync('noop')]
+  })
   .catch((err) => console.error(err));
